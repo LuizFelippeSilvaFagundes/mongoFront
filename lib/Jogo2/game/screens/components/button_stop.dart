f94d4fba1d4b01aaf1254aa306928e36721ce66f
Luiz Felippe <luizfelippefagundes06@gmail.com>
2024-06-09 21:57:33 -0300

    faltou um arquivo

---
+++ lib/Jogo2/game/screens/components/button_stop.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ButtonStop extends StatelessWidget {
+  final String label;
+  final Function() stop;
+
+  ButtonStop({required this.label, required this.stop}); // Alteração feita aqui
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: stop,
+      child: Container(
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: Color(0xFF786DEF),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 4),
+              blurRadius: 10,
+              color: Color(0xFFDBD3D3),
+            ),
+          ],
+          border: Border.all(
+            color: Color(0xFFDBD3D3),
+            width: 5,
+          ),
+        ),
+        child: Container(
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+              shadows: [
+                Shadow(
+                  offset: Offset(0, 1),
+                  blurRadius: 2,
+                  color: Colors.white12,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
